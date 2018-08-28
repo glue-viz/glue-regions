66351512e6aa6643336ae2c28a3395712c41db62
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-08-28 11:09:13 -0400

    rename

--- glue_regions/region_converter.py
+++ glue_regions/region_converter.py
@@ -4,6 +4,7 @@ from glue.config import data_factory
 from glue.core.data_factories import has_extension
 from glue.core.roi import CircularROI
 from glue.core.data import Data
+from glue.core.component import Component
 
 
 
@@ -12,5 +13,8 @@ def ds9_region(filename):
     reg = regions.read_ds9(filename)
 
 
+    comp = Component(np.ones(len(reg), dtype='bool'))
+    data = Data(label='Regions from {0}'.format(filename),
+                regions=component)
 
-
+    return data
--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ from setuptools import setup, find_packages
 
 entry_points = """
 [glue.plugins]
-glue-regions=glue-regions:setup
+glue_regions=glue_regions:setup
 """
 
 with open('README.rst') as infile:
     LONG_DESCRIPTION = infile.read()
 
-with open('glue-regions/version.py') as infile:
+with open('glue_regions/version.py') as infile:
     exec(infile.read())
 
-setup(name='glue-regions',
+setup(name='glue_regions',
       version=__version__,
       description='astropy regions support for glue',
       long_description=LONG_DESCRIPTION,
-      url="https://github.com/glue-viz/glue-plugin-template",
+      url="https://github.com/keflavich/glue-regions",
       author='',
       author_email='',
       packages = find_packages(),
