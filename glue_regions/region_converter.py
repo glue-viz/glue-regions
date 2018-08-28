3b62cdd2b8363e76292769d0476041443b54cb21
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-08-28 18:15:45 -0400

    fix subsetting

--- glue_regions/region_converter.py
+++ glue_regions/region_converter.py
@@ -1,9 +1,10 @@
 import numpy as np
+import os
 import regions
 
 from glue.config import data_factory
 from glue.core.data_factories import has_extension
-from glue.core.roi import CircularROI
+from glue.core.roi import CircularROI, PointROI
 from glue.core.data import Data
 from glue.core.component import Component
 
@@ -11,17 +12,18 @@ from glue.core.component import Component
 class RegionData(Data):
 
     def to_subset(self, wcs):
+        print("thing with to_subset: ",self)
         preg = [reg.to_pixel(wcs)
                 if hasattr(reg, 'to_pixel')
                 else reg
-                for reg in self['region']]
+                for reg in self['regions']]
 
         subsets = [CircularROI(xc=reg.center.x,
                                yc=reg.center.y,
                                radius=reg.radius)
                    if isinstance(reg, regions.CirclePixelRegion)
-                   else PixelROI(xc=reg.center.x,
-                                 yc=reg.center.y)
+                   else PointROI(x=reg.center.x,
+                                 y=reg.center.y)
                    for reg in preg]
         return subsets
 
@@ -32,6 +34,7 @@ def ds9_region(filename):
     reg = regions.read_ds9(filename)
 
     comp = Component(np.ones(len(reg), dtype='bool'))
-    data = RegionData(label='Regions from {0}'.format(filename), regions=reg)
+    data = RegionData(label='Regions: {0}'.format(os.path.split(filename)[-1]),
+                      regions=reg)
 
     return data
